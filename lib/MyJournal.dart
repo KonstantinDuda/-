cb9aca785b677abe0ec416582ab981ad7d67215f
KonstantinDuda <52273304+KonstantinDuda@users.noreply.github.com>
2020-03-25 20:48:49 +0200

    Шаблоны
    Добавил кнопку "Шаблоны". Добавил расширяемость элемента. Теперь выводится лишь часы и минуты, а не полная дата и время.

--- lib/MyJournal.dart
+++ lib/MyJournal.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:journal/MyDialog.dart';
+import 'package:journal/MyElement.dart';
 
 class MyJournal extends StatefulWidget {
-  MyJournal({Key key, /*this.title*/}) : super(key: key);
+  MyJournal({
+    Key key,
+    /*this.title*/
+  }) : super(key: key);
 
   //final String title;
 
@@ -11,58 +15,112 @@ class MyJournal extends StatefulWidget {
 }
 
 class _MyJournalState extends State<MyJournal> {
-  int _counter = 0;
+  int counter = 0;
+  List<MyElement> myElement = [];
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  addElement(String value) {
+    counter++;
+    var hour = DateTime.now().hour.toString();
+    var minute = DateTime.now().minute.toString();
+    var nowTime = hour + " : " + minute;
+    var year = DateTime.now().year;
+    var month = DateTime.now().month;
+
+    MyElement newElement = new MyElement(
+      id: counter,
+      time: nowTime, //DateTime.now().hour.toString(),
+      text: value,
+    );
+    myElement.add(newElement);
+  }
+
+  heightCalculate(String value) {
+    if (value.length > 100)
+      return 100.0;
+    else
+      return 50.0;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Журнал"),
+        title: Center(
+          child: Text("Журнал"),
+        ),
       ),
       body: Column(
         children: <Widget>[
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'Вы можете нажать на эту кнопку, для вызова диалогового окна',
-                ),
-                Text(
-                  '$_counter',
-                  style: Theme.of(context).textTheme.display1,
-                ),
-              ],
-            ),
-          ),
-          // Дальше написано повидение кнопки и его вид
           Container(
-            margin: EdgeInsets.all(10),
-            child: Align(
-              alignment: Alignment.centerRight,
-              child: RaisedButton(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(18.0)),
-                  color: Colors.blue,
-                  child: Icon(Icons.add),
-                  onPressed: () async {
-                    String value = await Navigator.push(
-                        context,
-                        PageRouteBuilder(
-                            opaque: false,
-                            pageBuilder: (context, _, __) => MyDialog()));
-                    if (value != '') {
-                      _incrementCounter();
-                    }
-                  }),
-            ),
-          )
+              child: Expanded(
+                  child: Container(
+                      child: ListView.builder(
+            itemCount: myElement.length,
+            itemBuilder: (context, index) {
+              return Container(
+                height: heightCalculate(myElement[index].text),
+                child: Card(
+                  child: Row(
+                    children: <Widget>[
+                      Text(myElement[index].time.toString()),
+                      Text(myElement[index].text),
+                    ],
+                  ),
+                ),
+              );
+            },
+          )))),
+          Row(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.all(10),
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: RaisedButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18.0)),
+                      color: Colors.blue,
+                      child: Text("Шаблоны"),
+                      onPressed: () async {
+                        String value = await Navigator.push(
+                            context,
+                            PageRouteBuilder(
+                                opaque: false,
+                                pageBuilder: (context, _, __) => MyDialog()));
+                        if (value != '') {
+                          setState(() {
+                            addElement(value);
+                          });
+                        }
+                      }),
+                ),
+              ),
+              Expanded(child: Container(),),
+              Container(
+                margin: EdgeInsets.all(10),
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: RaisedButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18.0)),
+                      color: Colors.blue,
+                      child: Icon(Icons.add),
+                      onPressed: () async {
+                        String value = await Navigator.push(
+                            context,
+                            PageRouteBuilder(
+                                opaque: false,
+                                pageBuilder: (context, _, __) => MyDialog()));
+                        if (value != '') {
+                          setState(() {
+                            addElement(value);
+                          });
+                        }
+                      }),
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
